fc8fbd55923af951606bb6feb69057f54f6db228
Milan Hauth <milahu@gmail.com>
2022-12-05 11:50:07 +0100

    add workarounds

--- demo/vite.config.js
+++ demo/vite.config.js
@@ -6,9 +6,23 @@ const assetsDir = '';
 const outputDefaults = {
   // remove hashes from filenames
   entryFileNames: `${assetsDir}[name].js`,
+
+  // import from parent folder
   // Invalid substitution "../shell" for placeholder "[name]" in "output.chunkFileNames" pattern, can be neither absolute nor relative path.
   chunkFileNames: `${assetsDir}[name].js`,
-  //chunkFileNames: (file) => { console.dir(file.name); throw new Error("x"); },
+
+  /*
+  // workarounds ...
+  chunkFileNames: (file) => {
+    //console.dir(file.name); throw new Error("x");
+    // Invalid pattern ".._index.js" for "output.chunkFileNames", patterns can be neither absolute nor relative paths. If you want your files to be stored in a subdirectory, write its name without a leading slash like this: subdirectory/pattern.
+    //return file.name.replace(/\//g, "_") + ".js" // docs/.._index.js
+    //return file.name.replace(/\//g, "_").replace(/^\./, "+") + ".js" // docs/+._index.js
+    //return file.name.replace(/\.\.\//g, "_") + ".js" // docs/_index.js
+    return file.name.replace(/\.\.\//g, ".parent/") + ".js" // docs/.parent/index.js
+  },
+  */
+
   assetFileNames: `${assetsDir}[name].[ext]`,
 }
 
